bc72070ef42b7583079649cb8719a905f6ac9e50
Angel seraphim <server@praniksl.ru>
2025-07-08 17:42:29 +0500

    Version

--- ColorTag/Plugin.cs
+++ ColorTag/Plugin.cs
@@ -17,7 +17,7 @@ namespace ColorTag
         public override string Name => "ColorTag";
         public override string Author => "angelseraphim.";
         public override string Description => "ColorTag";
-        public override Version Version => new Version(2, 0, 0);
+        public override Version Version => new Version(2, 0, 1);
         public override Version RequiredApiVersion => new Version(1, 0, 2);
 
         private string MainDirectory;
